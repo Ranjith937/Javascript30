846aa6047c60ec6fc7b12bf8c15eab67f2d3069b
Ranjith Kumar <padalaranjithkumar1@gmail.com>
2026-03-21 13:51:09 +0530

    Day 4 of Javascript30

--- 05 - Flex Panel Gallery/script.js
+++ 05 - Flex Panel Gallery/script.js
@@ -1,7 +1,9 @@
 const panels = document.querySelectorAll(".panel");
 
+// Here we have used toggle instead of add because we need to get the transitions when the panel gets clicked once and needs to go back to normal when it was clicked the second time.
+
 function toggleOpen(){
-    this.classList.toggle('open');
+    this.classList.toggle('open');  
 }
 function toggleActive(event){
     if(event.propertyName === 'flex-grow'){
